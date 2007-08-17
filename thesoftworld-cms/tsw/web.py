3354a4d7dcfbfbe8a6aeb90fbdf7df98dadb8997
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-08-17 16:16:39 -0400

    use vhost resource so that mailman knows its hostname

--- thesoftworld-cms/tsw/web.py
+++ thesoftworld-cms/tsw/web.py
@@ -1,6 +1,8 @@
+from zope.interface import implements
+
 from twisted.web import microdom, twcgi
 
-from nevow import static, rend, loaders, tags as T
+from nevow import static, rend, loaders, tags as T, inevow, vhost
 
 from tsw import RESOURCE
 
@@ -44,5 +46,27 @@ class TheSoftWorldPage(static.File):
             return twcgi.CGIDirectory(dir), segs[1:]
         return static.File.locateChild(self, ctx, segs)
 
+class VhostFakeRoot:
+    """
+    I am a wrapper to be used at site root when you want to combine 
+    vhost.VHostMonsterResource with nevow.guard. If you are using guard, you 
+    will pass me a guard.SessionWrapper resource.
+    """
+    implements(inevow.IResource)
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+    
+    def renderHTTP(self, ctx):
+        return self.wrapped.renderHTTP(ctx)
+        
+    def locateChild(self, ctx, segments):
+        """Returns a VHostMonster if the first segment is "VHOST". Otherwise
+        delegates to the wrapped resource."""
+        if segments[0] == "VHOST":
+            return vhost.VHostMonsterResource(), segments[1:]
+        else:
+            return self.wrapped.locateChild(ctx, segments)
+
 def root(directory):
-    return TheSoftWorldPage(directory)
+    return VhostFakeRoot(TheSoftWorldPage(directory))
+
