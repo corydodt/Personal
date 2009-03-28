9a06e728a6706e2b9ab347e6ebb1c82e816bdda3
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2009-03-28 16:26:11 -0700

    use goonsite/plugins as plugin dir for goonsite plugins

--- goonsite-cms/goonsite/plugins/__init__.py
+++ goonsite-cms/goonsite/plugins/__init__.py
@@ -1,3 +1,9 @@
 """
 The plugins directory
 """
+
+# add goonsite.plugins directories to the plugin-searchable paths
+from twisted.plugin import pluginPackagePaths
+__path__.extend(pluginPackagePaths(__name__))
+__all__ = []
+
