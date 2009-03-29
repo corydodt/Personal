2120ddb778be58e94e27aa1dd9a6e56197243f51
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2009-03-29 09:32:24 -0700

    halfrest javascript

---
+++ goonsite-cms/halfrest/halfrest.js
@@ -0,0 +1,32 @@
+document.observe('dom:loaded', function() {
+
+    var resizeIframe, bindEditBox;
+
+    bindEditBox = function() {
+        var edit;
+        edit = $$('.source')[0].down('.textareaHandle');
+        if (edit !== undefined) {
+            edit.observe('click', function () {
+                edit.next('.textareaMain').removeClassName('hidden');
+                edit.addClassName('hidden');
+            });
+        }
+    };
+    bindEditBox();
+
+});
+
+
+resizeIframe = function() {
+    var ifr, newHeight;
+    ifr = $$('iframe.displayDoc')[0];
+    if (ifr !== undefined) {
+        // adjust for margin and scrollbar
+        newHeight = ifr.contentDocument.height + 50;
+        new Effect.Morph(ifr, {
+                'style': 'height: ' + newHeight + 'px',
+                'duration': '0.5',
+                });
+    }
+};
+
