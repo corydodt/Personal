f5c989bdd83048a953d149528cd475c2bec89135
Cory Dodt <corymercurial@spam.goonmill.org>
2010-05-03 21:59:34 -0700

    G1 transcode for handbrake

---
+++ euler/euler_5.py
@@ -0,0 +1,31 @@
+"""
+euler.5
+
+2520 is the smallest number that can be divided by each of the numbers from 1
+to 10 without any remainder.
+
+What is the smallest number that is evenly divisible by all of the numbers
+from 1 to 20?
+"""
+# solved by manually eliminating any factors whose prime factors are already
+# included in a larger number in the 1..20 series
+print 11*13*14*17*18*19*20
+print 232792560./2
+print 232792560./3
+print 232792560./4
+print 232792560./5
+print 232792560./6
+print 232792560./7
+print 232792560./8
+print 232792560./9
+print 232792560./10
+print 232792560./11
+print 232792560./12
+print 232792560./13
+print 232792560./14
+print 232792560./15
+print 232792560./16
+print 232792560./17
+print 232792560./18
+print 232792560./19
+print 232792560./20
