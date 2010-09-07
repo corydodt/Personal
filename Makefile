ac9231ea9cf06f4991077c7a0703fe721f94cb77
Cory Dodt <corymercurial@spam.goonmill.org>
2010-09-07 00:31:21 -0700

    updated cory d's flickr thing for new flickr

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 corydoctorow.min.js:
-	@echo -n 'javascript:(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement("script");c.type="text/javascript";c.src="http://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,"1.4.2",function($,L){'
-	@printf '%s();});' `cat corydoctorow.js | tr -d $$'\n' | perl -pne 's/\ /%20/g'`
+	@echo -n 'javascript:(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement("script");c.type="text/javascript";c.src="http://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,"1.4.2",function($$,L){'
+	@cat corydoctorow.js | perl -pnle 's/^\s+//'
+	@echo '});'
--- corydoctorow.js
+++ corydoctorow.js
@@ -1,32 +1,30 @@
-(function () {
-        var title, link, username, ccTag, ccShort, ccName, ccVer;
-        licenses = {
-            'by': 'Attribution',
-            'by-sa': 'Attribution Share-Alike',
-            'by-nd': 'Attribution No-Derivative-Works',
-            'by-nc': 'Attribution Non-Commercial',
-            'by-nc-sa': 'Attribution Non-Commercial Share-Alike',
-            'by-nc-nd': 'Attribution Non-Commercial No-Derivative-Works'
-        };
+var title, link, username, ccURL, ccShort, ccName, ccVer;
+licenses = {
+    'by': 'Attribution',
+    'by-sa': 'Attribution Share-Alike',
+    'by-nd': 'Attribution No-Derivative-Works',
+    'by-nc': 'Attribution Non-Commercial',
+    'by-nc-sa': 'Attribution Non-Commercial Share-Alike',
+    'by-nc-nd': 'Attribution Non-Commercial No-Derivative-Works'
+};
 
 
-        ccTag = $('a[rel=license cc:license]:eq(0)').attr('href');
+ccURL = $('a[rel=license cc:license]:eq(0)').attr('href');
 
-        if (!ccTag) {
-            alert('Does not appear to be CC-licensed.\n:(');
-        };
+if (!ccURL) {
+    alert('Does not appear to be CC-licensed.\n:(');
+};
 
-        ccShort = new RegExp('.*creativecommons.org/licenses/(.*?)/(.*?)/.*').exec(ccTag);
+ccShort = new RegExp('.*creativecommons.org/licenses/(.*?)/(.*?)/.*').exec(ccURL);
 
-        ccName = licenses[ccShort[1]];
+ccName = licenses[ccShort[1]];
 
-        ccVer = ccShort[2];
+ccVer = ccShort[2];
 
-        title = $('meta[name=title]:eq(0)').attr('content');
-        link = $('link[rel=canonical]:eq(0)').attr('href');
-        username = new RegExp('.*flickr.com/photos/(.*?)/.*').exec(link)[1];
+title = $('meta[name=title]:eq(0)').attr('content');
+link = $('link[rel=canonical]:eq(0)').attr('href');
+username = new RegExp('.*flickr.com/photos/(.*?)/.*').exec(link)[1];
 
-        alert('(<i>Image: <a href="' + link + '">' + 
-                title + '</a>, a Creative Commons <a href="' + ccURL + '">' + 
-                ccName + ' (' + ccVer + ')</a> image from ' + username + '\'s photostream</i>)');
-})
+alert('(<i>Image: <a href="' + link + '">' + 
+        title + '</a>, a Creative Commons <a href="' + ccURL + '">' + 
+        ccName + ' (' + ccVer + ')</a> image from ' + username + '\'s photostream</i>)');
