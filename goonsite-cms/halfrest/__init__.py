30c7a73b7a0575bf04cb3f4ca4a1c3b6f414a87a
Cory Dodt <corymercurial@spam.goonmill.org>
2011-02-13 20:54:57 -0800

    fix date14 column

--- goonsite-cms/halfrest/__init__.py
+++ goonsite-cms/halfrest/__init__.py
@@ -13,5 +13,5 @@ STYLESHEET     =   '/static/character.css'
 CHARSHEET_T    =   open(RESOURCE('Charsheet_template.txt')).read()
 CREATE_SCRIPT  =   """  
 CREATE TABLE document (id INTEGER PRIMARY KEY, text VARCHAR, who VARCHAR,
-               date14 char(14));
+               date14 INTEGER);
 """
